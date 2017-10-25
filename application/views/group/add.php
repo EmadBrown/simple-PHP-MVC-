1f7090c2c8c7e0501cfe368a32747ba4246291dc
EmadBrown <emadbrown2013@gmail.com>
2017-10-25 10:35:37 +0200

    Create view group (index + add + view) pages

---
+++ application/views/group/add.php
@@ -0,0 +1,11 @@
+<div class="container">
+         <div class="starter-template">
+                 <form method="POST" action="addGroup">
+                              <div class="form-group">
+                                    <label for="disabledTextInput">New Group Name</label>
+                                    <input type="text" id="name" name="name" class="form-control" placeholder="New Name">
+                              </div>
+                              <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+                </form>
+         </div>
+</div>
---
+++ application/views/group/index.php
@@ -0,0 +1,29 @@
+<div class="container">
+         <div class="starter-template">
+                    <!-- add Group form -->
+
+                    <table class="table table-striped">
+                            <thead>
+                            <tr>
+                                <td>Id</td>
+                                <td>Group Name</td>
+
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <?php foreach ($groups as $group ) { ?>
+                                <tr>
+                                    <td><?php if (isset($group->id)) echo $group->id; ?></td>
+                                    <td><?php if (isset($group->name)) echo $group->name; ?></td>
+                                    <td>
+                                            <a href="<?php echo URL . 'group/view/' . $group->id; ?>" class="btn btn-success"> View </a>
+                                            <a href="<?php echo URL . 'group/deleteGroup/' . $group->id; ?>" class="btn btn-danger"> Delete </a>
+                                    </td>
+                                </tr>
+                            <?php } ?>
+                            </tbody>
+                   </table>
+         </div>
+
+</div>
+ 
