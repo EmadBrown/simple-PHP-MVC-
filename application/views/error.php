f4ff2f7fd48589947628f56b728c009bb0b5e77e
EmadBrown <emadbrown2013@gmail.com>
2017-10-25 10:38:15 +0200

    Create view error page

---
+++ application/views/error.php
@@ -0,0 +1,27 @@
+<div class="container">
+     
+                 <!-- This is a workaround for a bug in Chrome. See http://crbug.com/660929 for details. -->
+                <div class="page-container page-container-responsive">
+                    <div class="row space-top-8 space-8 row-table">
+                        <div class="col-5 col-middle">
+                          <h1 class="text-jumbo text-ginormous">Oops!</h1>
+                          <h2>We can't seem to find the page you're looking for.</h2>
+                          <h3>Error code: 404</h3>
+                          <ul class="list-unstyled">
+                                <li>Here are some helpful links instead:</li>
+                                <li> <a  href="<?php echo URL;?>group">Groups <span class="sr-only">(current)</span></a></li>
+                                <li><a  href="<?php echo URL;?>group/add">Add New Group</a></li>
+                                <li> <a   href="<?php echo URL;?>item">Items</a></li>
+                                <li><a   href="<?php echo URL;?>item/add">Add New Item</a></li>
+                                <li><a href="https://Emad-Albouni@bitbucket.org/Emad-Albouni/">Bitbucket</a></li>
+                          </ul>
+                        </div>
+                        <div class="col-5 col-middle text-center">
+                          <img src="https://a0.muscache.com/airbnb/static/error_pages/404-Airbnb_final-d652ff855b1335dd3eedc3baa8dc8b69.gif" width="313" height="428" alt="Girl has dropped her ice cream.">
+                        </div>
+                      </div>
+                    </div>
+                </div>
+</div>
+        
+ 
